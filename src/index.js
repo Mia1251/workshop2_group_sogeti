f576e5aff56ca1ae9b6134968a5791dfa7d844a7
Mia1251 <svanteomax@gmail.com>
2022-02-16 16:29:53 +0100

    Part 1 done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import DemoRouter from './components/DemoRouter';
 
-ReactDOM.render(<div>Hello everybody! Hope you're feeling fine!</div>, document.getElementById("root"));
+ReactDOM.render(<DemoRouter />, document.getElementById("root"));
